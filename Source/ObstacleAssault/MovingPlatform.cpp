ba39c9b2469cbb88ca220c4fc3f3b789284392ac
jaejune Yun <jjune0427@naver.com>
2022-10-06 22:57:02 +0900

    Moving Platform 제작

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -15,7 +15,10 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
+	MyX = MyVector.X;
+	//MyVector.Set(-13900.0, -4330.0, 4056.0);
 	
+	SetActorLocation(MyVector);
 }
 
 // Called every frame
@@ -23,5 +26,20 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector LocalVector;
+
+	MyVector.Z = MyVector.Z + 0.2;
+	MyVector.Y = MyVector.Y + 1;
+
+	LocalVector = MyVector;
+	SetActorLocation(LocalVector);
+
+	//Move platform forward;
+		//Get Current location;
+		//Add vector to that location
+		//Set the location
+	//Send platform back if gone too far
+		//Check how far we've moved
+		//Reverse direction of motion if gone too far
 }
 
