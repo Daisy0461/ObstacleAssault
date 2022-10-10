384cac05af4ca7cd69a8c3c8151b433fdd28efe0
jaejune Yun <jjune0427@naver.com>
2022-10-10 22:47:37 +0900

    Complete

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -23,24 +23,32 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	//Move platform forward;
-		//Get Current location;
-	FVector CurrentLocation = GetActorLocation();
-		//Add vector to that location
-		CurrentLocation = CurrentLocation + PlatformVelocity*DeltaTime;
-		//Set the location
-		SetActorLocation(CurrentLocation);
-	//Send platform back if gone too far
-		//Check how far we've moved
-		DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
-		
-		//Reverse direction of motion if gone too far
-		if(DistanceMoved >= MoveDistance)
-		{
-			FVector MoveDirection = PlatformVelocity.GetSafeNormal();			//Platform이 가는 방향을 넣음
-			StartLocation += MoveDirection * MoveDistance;						//시작지점을 가는 처음위치 + 가는방향(Normal Vector) * 거리로 해서 일정한 곳을 지정함
-			PlatformVelocity = -PlatformVelocity;								//PlatformVelocity를 거꾸로가게 만든다.
-			SetActorLocation(StartLocation);									//시작지점을 현재 도착한 지점(MoveDistance보다 현재 크니까 지금이 터닝포인트)로 지정한다.
-		}
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);	
 }
 
+void AMovingPlatform::MovePlatform(float DeltaTime){
+	if(ShouldPlatformReturn())
+	{
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();			//Platform이 가는 방향을 넣음
+		StartLocation += MoveDirection * MoveDistance;						//시작지점을 가는 처음위치 + 가는방향(Normal Vector) * 거리로 해서 일정한 곳을 지정함
+		PlatformVelocity = -PlatformVelocity;								//PlatformVelocity를 거꾸로가게 만든다.
+		SetActorLocation(StartLocation);									//시작지점을 현재 도착한 지점(MoveDistance보다 현재 크니까 지금이 터닝포인트)로 지정한다.
+	}else{
+		CurrentLocation = GetActorLocation();complete
+		CurrentLocation = CurrentLocation + PlatformVelocity*DeltaTime;
+		SetActorLocation(CurrentLocation);
+	}	
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime){			
+	AddActorLocalRotation(RotationVelocity * DeltaTime);
+}
+
+bool AMovingPlatform::ShouldPlatformReturn() const{			//const로 된 함수는 const로 만들어진 함수만 부를 수 있다. GetDistanceMoved는 class안에 있는 값을 수정하진 않지만 const를 지우면 에러가 뜬다.
+	return GetDistanceMoved() > MoveDistance;
+}
+
+float AMovingPlatform::GetDistanceMoved() const{
+	return FVector::Dist(StartLocation, GetActorLocation());
+}
